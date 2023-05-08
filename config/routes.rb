7cc85c7fa8cfc960c7f0a5d528882109032ddc20
Eli Da Costa <costa141@hotmail.co.uk>
2023-05-08 21:21:16 +0100

    non commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    # root to: 'lists#home'
+    resources :lists
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Movie.destroy_all
+50.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Movies::HarryPotter.quote,
+    poster_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.polygon.com%2F23668971%2Fharry-potter-reboot-hbo-series-tv&psig=AOvVaw0fZgz-1WAp6zifO1muctWH&ust=1683473537481000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPiL9OOB4f4CFQAAAAAdAAAAABAE",
+    rating: rand(1..5)
+
+  )
+  puts movie
+
+end
